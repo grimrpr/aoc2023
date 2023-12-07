3b70d5b19ce6fbf171675cf111729664bec4fe43
grimrpr <benjamin.aschenbrenner@gmail.com>
2023-12-07 08:42:14 +0100

    Add day3 initial

---
+++ src/days/day3.rs
@@ -0,0 +1,11 @@
+// mod day2
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+
+pub fn print_answer() {
+    let reader = BufReader::new(File::open("data/input_day3").unwrap());
+}
