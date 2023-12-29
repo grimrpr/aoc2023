5df988a4e5ceea0299ed9d3ca2169518d12977da
Benjamin Aschenbrenner <benjamin.aschenbrenner@gmail.com>
2023-12-29 16:34:14 +0100

    Add day11 part1 result

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -10,11 +12,40 @@ pub fn print_answer() {
     let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
     let width = lines[0].len();
     let height = lines.len();
-    let galaxies: Vec<Galaxy> = lines
+    let mut galaxies: Vec<Galaxy> = lines
         .iter()
         .enumerate()
-        .flat_map(|(row_idx, line)| {
-            repeat(row_idx).zip(line.match_indices('#'))
-        }).map(|(row_idx, (col_idx, _))| Galaxy(row_idx, col_idx))
+        .flat_map(|(row_idx, line)| repeat(row_idx).zip(line.match_indices('#')))
+        .map(|(row_idx, (col_idx, _))| Galaxy(row_idx, col_idx))
         .collect();
+
+    let remap_rows: HashMap<usize, usize> = HashMap::from_iter(
+        BTreeSet::<usize>::from_iter(galaxies.iter().map(|Galaxy(row, _)| *row))
+            .into_iter()
+            .zip(0..height)
+            .map(|(row_galaxy, row_idx)| (row_galaxy, row_galaxy + row_galaxy - row_idx)),
+    );
+    let remap_cols: HashMap<usize, usize> = HashMap::from_iter(
+        BTreeSet::<usize>::from_iter(galaxies.iter().map(|Galaxy(_, col)| *col))
+            .into_iter()
+            .zip(0..width)
+            .map(|(col_galaxy, col_idx)| (col_galaxy, col_galaxy + col_galaxy - col_idx)),
+    );
+
+    // expand the universe
+    for galaxy in &mut galaxies {
+        galaxy.0 = remap_rows[&galaxy.0];
+        galaxy.1 = remap_cols[&galaxy.1];
+    }
+
+    // sum of shortest paths between galaxy pairs
+    let mut sum_of_lengths = 0usize;
+    for i in 0..galaxies.len() {
+        for j in (i + 1)..galaxies.len() {
+            sum_of_lengths += galaxies[i].0.abs_diff(galaxies[j].0) + galaxies[i].1.abs_diff(galaxies[j].1)
+        }
+    }
+
+    println!("Sum of lengths {}", sum_of_lengths);
+
 }
