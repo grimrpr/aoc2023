b83817a336b0e0f9bf2f941d7811d59d3c5f4a84
grimrpr <benjamin.aschenbrenner@gmail.com>
2024-03-30 18:38:02 +0100

    Add day12 initial

---
+++ src/days/day12.rs
@@ -0,0 +1,8 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+pub fn print_answer() {
+    let reader = BufReader::new(File::open("data/input_day11").unwrap());
+    let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+}
